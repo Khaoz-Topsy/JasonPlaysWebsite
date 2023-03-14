58c0b0f87c825af3613527dd0828c6bf23d21772
Kurt Lourens <kurt.lourens@outlook.com>
2023-03-14 21:17:15 +0100

    ✨ Handle multiple leaderboards

--- webpack/lib/custom.js
+++ webpack/lib/custom.js
@@ -2,6 +2,7 @@ import * as skrollr from './skrollr.min.js';
 
 window.onload = function () {
     parallax();
+    setUpTabs();
 };
 
 function parallax() {
@@ -17,4 +18,41 @@ function parallax() {
 
     var sceneMaster = document.getElementById('scene-master');
     var parallaxInstanceMaster = new Parallax(sceneMaster);
+}
+
+function handleTabClick() {
+    if ([...this.classList].includes('disabled')) return;
+
+    const tabBtns = document.getElementsByClassName('tab-item');
+    for (const tabBtn of tabBtns) {
+        tabBtn.classList.remove('active');
+    }
+
+    const tabViews = document.getElementsByClassName('tab-content');
+    for (const tabView of tabViews) {
+        tabView.classList.remove('active');
+    }
+
+    setCurrentTab(this);
+    // setTimeout(() => setCurrentTab(this), 250);
+}
+
+function setCurrentTab(tabItemRef) {
+    tabItemRef.classList.add('active');
+
+    // setTimeout(() => {
+    const query = `.${tabItemRef.id}.tab-content`;
+    const tabViewWrapper = document.querySelector(query);
+    if (tabViewWrapper == null) return;
+    tabViewWrapper.classList.add('active');
+    // }, 250);
+}
+
+function setUpTabs() {
+    const tabItems = document.getElementsByClassName('tab-item');
+    for (const tabItem of tabItems) {
+        tabItem.addEventListener("click", handleTabClick)
+    }
+
+    setCurrentTab(tabItems[0])
 }
